5b78416c7556d09b41a8f7c2ceb702560bcb3b2e
Arda Balkir <abalkir2014@gmail.com>
2022-07-06 18:14:27 +0100

    Update RNAStrandCaclulatorGUI.py

--- RNAStrandCaclulatorGUI.py
+++ RNAStrandCaclulatorGUI.py
@@ -1,5 +1,4 @@
 from tkinter import * 
-
 from more_itertools import *
 
 root = Tk()
@@ -8,25 +7,21 @@ root.title("DNA Strand Varience Calculator")
 root.geometry("320x220")
 
 paddings = {'padx': 15, 'pady': 15}
-
-
+#creating rna sample database as a list
 dnaList = ["DNASAMPLETCAG"]
-
-
+#setting entry
 e = Entry(root, width=32)
 e.grid(column=0,row=0, sticky=W, **paddings)
 
 dna1 = StringVar()
-
-
 dna2 = StringVar()
 
-
+#creating the dropdown menus for comparison
 dnaDrop1 = OptionMenu(root, dna1, *dnaList)
 dnaDrop1.grid(column=0, row=1, sticky=W, **paddings)
 dnaDrop2 = OptionMenu(root, dna2, *dnaList)
 dnaDrop2.grid(column=0, row=2, sticky=W, **paddings)
-
+#Function to take the entry and add it as a rna strand
 def addCl():
     dnaList.append(e.get())
     dnaDrop1 = OptionMenu(root, dna1, *dnaList)
@@ -41,7 +36,7 @@ def cmpCl():
     if len(d1) != len(d2):
         eqlbl = Label(root, text="The strands are not of matching size").grid(column=0,row=3)
         return 
-
+    #Grouping the strings into mini lists of four to simulate the structure of the rna better
     d1 = list(chunked((list(d1)), 4))
     d2 = list(chunked((list(d2)), 4))
 
@@ -53,14 +48,12 @@ def cmpCl():
     varience = varCnt / len(d1) * 100
 
     vrnStr = f"Common gene percentage is {varience}%"
-
     eqlbl = Label(root, text=vrnStr).grid(column=0,row=3)
 
+    #add and compare buttons here!
 addButton = Button(root, text="Add", command=addCl)
 addButton.grid(column=1,row=1, sticky=W, **paddings) 
 cmpButton = Button(root, text="Compare", command=cmpCl)
 cmpButton.grid(column=1,row=2, sticky=W, **paddings) 
-
-
 
 root.mainloop()
